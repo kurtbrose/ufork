d4e6e3da1a9c90c15b4e690e0c6da8d4ff57e244
Serhii Khortiuk <khortiukserhii@ukr.net>
2020-10-20 16:55:23 +0300

    Fixed `test_wsgiref_hello`.

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -3,10 +3,11 @@ import threading
 import time
 import six.moves.urllib.request, six.moves.urllib.error, six.moves.urllib.parse
 
-from tests.utils import check_leaked_workers
 from ufork import Arbiter
 
 from wsgiref.simple_server import make_server
+
+from tests.utils import check_leaked_workers
 
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 7777
@@ -35,18 +36,15 @@ def test_wsgiref_hello():
         child_pre_exit=httpd.shutdown,
         size=1
     )
-    arbiter_thread = threading.Thread(target=arbiter.run, kwargs={"repl": False})
     arbiter.spawn_thread()
-    arbiter_thread.daemon = True
-    arbiter_thread.start()
-    time.sleep(10)  # Todo: Find another way to wait until server is ready to accept requests.
-    assert len(arbiter.workers) == 1
-    response = six.moves.urllib.request.urlopen('http://{}:{}'.format(SERVER_HOST, SERVER_PORT)).read()
-    if six.PY3:
-        assert response == b'Hello World\n'
-    else:
-        assert response == 'Hello World\n'
-    arbiter.stopping = True
-    arbiter_thread.join()
-    time.sleep(3)
-    check_leaked_workers(arbiter)
+    time.sleep(3)  # Todo: Find another way to wait until server is ready to accept requests.
+    try:
+        response = six.moves.urllib.request.urlopen('http://{}:{}'.format(SERVER_HOST, SERVER_PORT)).read()
+        if six.PY3:
+            assert response == b'Hello World\n'
+        else:
+            assert response == 'Hello World\n'
+    finally:
+        arbiter.stop()
+        arbiter.thread.join()
+        check_leaked_workers(arbiter)
