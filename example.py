fd7d8634d7f646bb155ac967ac202865fffc6b4f
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-11-10 23:30:29 -0800

    add example.py with a basic wsgi server

---
+++ example.py
@@ -0,0 +1,33 @@
+# install ufork and run with "python example.py"
+
+import os
+import threading
+from wsgiref.simple_server import make_server
+
+from ufork import Arbiter
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 7777
+
+
+def wsgi_app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+
+    start_response(status, headers)
+    yield ("Hello, World! (from PID %s)\n" % os.getpid()).encode('utf-8')
+
+
+httpd = make_server(SERVER_HOST, SERVER_PORT, wsgi_app)
+
+
+def start_server():
+    print('starting server on http://%s:%s' % (SERVER_HOST, SERVER_PORT))
+    arbiter = Arbiter(
+        post_fork=httpd.serve_forever,
+        child_pre_exit=httpd.shutdown,
+        size=2
+    )
+    arbiter.run()
+
+start_server()
